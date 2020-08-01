355727818226a1f511807e160788909deac6a5ce
georgezh1996 <georgezh@umich.edu>
2020-08-01 16:24:40 -0500

    Update hangman.py
    created helper functions

--- Python Scripts/hangman/hangman.py
+++ Python Scripts/hangman/hangman.py
@@ -4,6 +4,10 @@ Created on Sun Jul 19 20:27:24 2020
 
 @author: George Zhou
 """
+from random import choice
+from re import *
+
+
 #hangman picturse
 with(open('hangman.txt','r')) as f:
         hangMan=f.read().split(',')
@@ -12,26 +16,62 @@ with(open('hangman.txt','r')) as f:
 with(open('wordbank.txt','r')) as f:
         wordBank=f.read().split('\n')    
 
-from random import choice
-from re import *
-
+#modify hyphen structure
 def wordModify(index,wordSTR,guess):
     charList=list(wordSTR)
     for i in index:
         charList[i]=guess
     return(''.join(charList))
-
+#display letters used 
 def displayLetters(word,letters):
     lettersSort=sorted([i for i in letters if i not in word])
     if not lettersSort:
         return("")
     else:
         return(', '.join(lettersSort))
-   
+#check if guess is valid 
+def checkLetter(lettersUsed):  
+    guess=input('Please guess a letter: ').lower()
+    global message
+    if len(guess)!=1 or guess.isalpha()==False:
+          message="Please pick an appropriate letter."
+          return('none',message)
+    elif guess in lettersUsed:
+        message='Please pick a letter that was not used before!'
+        return('none',message)
+    else:
+        message='Have fun!'
+        return(guess,message)
+#check if guess is in word, and show results 
+def indiceCheck(word,wordHyphen,guess,limbs,lettersUsed):
+    indices = [i for i, a in enumerate(word) if a == guess]
+    if not indices:
+        limbs+=1
+        lettersUsed.append(guess)
+        print("\n"*50)
+        return(lettersUsed,wordHyphen,limbs)
+    else:
+        wordHyphen=wordModify(indices, wordHyphen, guess)
+        lettersUsed.append(guess)
+        print("\n"*50)
+        return(lettersUsed,wordHyphen,limbs)
+#check if you win or lose 
+def endGame(limbs,wordHyphen,word,guess):
+    if limbs==6:
+        print("Sorry you lose! The phrase was: "+word)
+        print(hangMan[6]+'\n')
+        print(wordHyphen+'\n')
+        print('Please guess a letter: '+guess)
+        return(True)
+    elif word==wordHyphen:
+        print('Congragulations, you won!')
+        print(hangMan[limbs]+'\n')
+        print(wordHyphen)
+         
 def hangman(wordList):
-    message='Have fun!'
     limbs=0
     done=False
+    message='Have fun!'
     #list for letters used
     lettersUsed=[]
     #make all letters lowercase 
@@ -42,35 +82,11 @@ def hangman(wordList):
        print(hangMan[limbs]+'\n')
        print(wordHyphen)
        print('letters used: '+ displayLetters(word, lettersUsed)) #display letters nicely
-       guess=input('Please guess a letter: ').lower()
-       if len(guess)!=1 or guess.isalpha()==False:
-           message="Please pick an appropriate letter."
-           print("\n"*50)
-       elif guess in lettersUsed:
-           message='Please pick a letter that was not used before!'
-           print("\n"*50)
-       else:
-            indices = [i for i, a in enumerate(word) if a == guess]
-            if not indices:
-                limbs+=1
-                lettersUsed.append(guess)
-                message='Have fun!'
-                print("\n"*50)
-                if limbs==6:
-                    print("Sorry you lose! The phrase was: "+word)
-                    print(hangMan[6]+'\n')
-                    print(wordHyphen+'\n')
-                    print('Please guess a letter: '+guess)
-                    done=True
-            else:
-                #show letter position in word
-                wordHyphen=wordModify(indices, wordHyphen, guess)
-                lettersUsed.append(guess)
-                print("\n"*50)
-                if wordHyphen==word:
-                    print('Congragulations, you won!')
-                    print(hangMan[limbs]+'\n')
-                    print(wordHyphen)
-                    done=True
+       guess,message=checkLetter(lettersUsed)
+       if guess != 'none':
+           lettersUsed,wordHyphen,limbs=indiceCheck(word, wordHyphen, guess, limbs, lettersUsed)
+           if wordHyphen==word or limbs==6:
+               done=endGame(limbs, wordHyphen,word,guess)
+
 hangman(wordBank)
-    
+                   
