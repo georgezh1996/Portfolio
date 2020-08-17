2cf5d60a856aafbc1e9f94bf15b13725a3df46f1
georgezh1996 <georgezh@umich.edu>
2020-08-17 14:46:48 -0500

    Update modelFitting.R
    Made some of the processed into models to reduce lines of code and increase readability

--- tennis ML/Model fitting/modelFitting.R
+++ tennis ML/Model fitting/modelFitting.R
@@ -5,11 +5,7 @@ library(MASS)
 library(tree)
 atpMatchesClean=read_rds('atpMatchesClean.RDS')
 #create variable to show if servers faced break point or not 
-atpMatchesClean<-atpMatchesClean %>% mutate(winner=ifelse(winner==1,1,0)) %>% 
-  mutate(bkFace=ifelse(bkPtFc1==0&bkPtFc2==0,'none','wait')) %>% 
-  mutate(bkFace=ifelse(bkPtFc1!=0&bkPtFc2==0,2,bkFace)) %>% 
-  mutate(bkFace=ifelse(bkPtFc1==0&bkPtFc2!=0,1,bkFace)) %>% 
-  mutate(bkFace=ifelse(bkPtFc1!=0&bkPtFc2!=0,'both',bkFace)) %>% 
+atpMatchesClean<-atpMatchesClean %>% mutate(winner=ifelse(winner==1,1,0)) 
   mutate(winner=factor(winner))
 
 #diff
@@ -18,100 +14,72 @@ atpMatchesDiff<-atpMatchesClean %>%
   mutate(winner=factor(winner))
 
 #no diff
-linDep<-c('match_id','loser','retFstWon','retSndWon','points','bkPtsSave','netPts_won')
+linDep<-c('match_id','loser','retFstWon','retSndWon','points','bkPtsSave','netPts_won','bkSave')
 atpMatchesNoDiff<-atpMatchesClean %>% 
   dplyr::select(-contains(c('diff',linDep)))
 
 #both diff and no diff
 atpMatchesCleanBoth<-atpMatchesClean %>% dplyr::select(-contains(linDep))
-
 set.seed(2019)
 
-
-
-#diff
+#training index
 trainIndex<-sample(1:nrow(atpMatchesDiff),trunc(.8*nrow(atpMatchesDiff)))
+#fit models based on method used and data frame used 
+models<-function(df,index,method_used){
+  train<-df[index,]
+  test<-df[-index,]
+  regFit<-regsubsets(winner~.,data=train,method = method_used,really.big = T,nvmax=(ncol(df)-1))
+  return(regFit)
+}
+#pick coefficients used 
+modelCoefficients<-function(regFit){
+  bicResults=data.frame(nVar=1:length(summary(regFit)$cp),BIC=summary(regFit)$bic)
+  nCoef<-which(bicResults$BIC==min(bicResults$BIC))
+  return(coef(regFit,nCoef))
+  print(graph1)
+}
+#evaluate based on types of models used 
+modelEvaluation<-function(coefs,df,index){
+  coefPlus<-str_c(names(coefs[-1]),collapse = '+')
+  coefFormula<-str_c('factor(winner)~',coefPlus,collapse = "")
+  #fit model
+  logModel<-glm(formula(coefFormula),data = df,subset = index,family='binomial')
+  ldaModel<-lda(formula(coefFormula),data=df,subset = index)
+  qdaModel<-qda(formula(coefFormula),data=df,subset = index)
+  treeModel<-tree(formula(coefFormula),data=df,subset = index)
+  #predict based on model 
+  logPredict<-round(predict(logModel,df[-index,],type='response'))
+  ldaPredict<-predict(ldaModel,df[-index,],type='response')$class
+  qdaPredict<-predict(qdaModel,df[-index,],type='response')$class
+  treePredict<-predict(treeModel,df[-index,],type='class')
+  #how many correct 
+  logResults<-mean(logPredict==df$winner[-index])
+  ldaResults<-mean(ldaPredict==df$winner[-index])
+  qdaResults<-mean(qdaPredict==df$winner[-index])
+  treeResults<-mean(treePredict==df$winner[-index])
+  
+  error<-round(1-c(logResults,ldaResults,qdaResults,treeResults),3)
+  names(error)<-c('Logistic','LDA','QDA','DecisionTree')
+  return(error)
+}
+#gather coefficients
+regFitDiff<-models(atpMatchesDiff,trainIndex,'exhaustive')
+regFitNoDiff<-models(atpMatchesNoDiff,trainIndex,'forward')
+regFitBoth<-models(atpMatchesCleanBoth,trainIndex,'forward')
+#find coefficients
+coefsDiff<-modelCoefficients(regFitDiff)
+coefsNoDiff<-modelCoefficients(regFitNoDiff)
+coefsBoth<-modelCoefficients(regFitBoth)
+#fit and evaluate models 
+coefsDiffResults<-modelEvaluation(coefsDiff,atpMatchesDiff,trainIndex)
+coefsNoDiffResults<-modelEvaluation(coefsNoDiff,atpMatchesNoDiff,trainIndex)
+coefsBothResults<-modelEvaluation(coefsBoth,atpMatchesCleanBoth,trainIndex)
 
-train=atpMatchesDiff[trainIndex,]
-test=atpMatchesDiff[-trainIndex,]
-regFit<-regsubsets(winner~.,data=train,method = 'exhaustive',really.big = T,nvmax=12)
+results<-rbind(coefsDiffResults,coefsNoDiffResults,coefsBothResults)
+results<-data.frame(nVar=c(8,15,9),results)
+row.names(results)<-c('Difference Only','Individual Stats','Both')
+knitr::kable(results)
 
 
-bicResults=data.frame(nVar=1:length(summary(regFit)$cp),BIC=summary(regFit)$bic)
 
 
-bicResults %>%mutate(color = min(BIC) == BIC) %>% 
-  ggplot(aes(x=nVar,y=BIC)) +
-  geom_line()+
-  geom_point(aes(color=color)) +
-  scale_x_continuous(breaks=1:12)+
-  scale_color_manual(values=c(NA,'red'))+
-  theme_light()+
-  theme(legend.position = 'none')
-coef(regFit,7)
-logModel<-train %>% 
-  glm(winner~fstSvWonDiff+sndSvWonDiff+dfDiff+winDiff+UEdiff+forcedPointsDiff+unRetDiff,family = 'binomial',data=.)
-ldaModel<-train %>% lda(winner~fstSvWonDiff+sndSvWonDiff+dfDiff+winDiff+UEdiff+forcedPointsDiff+unRetDiff,data = .)
-qdaModel<-train %>% qda(winner~fstSvWonDiff+sndSvWonDiff+dfDiff+winDiff+UEdiff+forcedPointsDiff+unRetDiff,data = .)
-
-treeModel<-tree(winner~fstSvWonDiff+sndSvWonDiff+dfDiff+winDiff+UEdiff+forcedPointsDiff+unRetDiff,data = train)
-plot(treeModel)
-text(treeModel,splits = T,digits = 3)
-
-pLog<-round(predict(logModel,test,type='response'))
-mean(pLog==test$winner)
-
-pLDA<-predict(ldaModel,test,type='response')$class
-mean(pLDA==test$winner)
-
-pTree<-predict(treeModel,test,type='class')
-mean(pTree==test$winner)
-
-pQDA<-predict(qdaModel,test,type='response')$class
-mean(pQDA==test$winner)
-
-# no diff
-
-
-trainNoDiff=atpMatchesNoDiff[trainIndex,]
-testNoDiff=atpMatchesNoDiff[-trainIndex,]
-
-regFitNoDiff<-regsubsets(winner~.,data=trainNoDiff,method = 'forward',really.big = T,nvmax=30)
-summary(regFitNoDiff)$bic
-
-bicResultsNoDiff=data.frame(nVar=1:length(summary(regFitNoDiff)$bic),BIC=summary(regFitNoDiff)$bic)
-
-
-bicResultsNoDiff %>%mutate(color = min(BIC) == BIC) %>% 
-  ggplot(aes(x=nVar,y=BIC)) +
-  geom_line()+
-  geom_point(aes(color=color)) +
-  scale_x_continuous(breaks=1:29)+
-  scale_color_manual(values=c(NA,'red'))+
-  theme_light()+
-  theme(legend.position = 'none')
-names(coef(regFitNoDiff,15))
-logModel<-train %>% 
-  glm(winner~fstSvWonDiff+sndSvWonDiff+dfDiff+winDiff+UEdiff+bkWonDiff+bkPtFcDiff+forcedPointsDiff+unRetDiff,family = 'binomial',data=.)
-ldaModel<-train %>% lda(winner~fstSvWonDiff+sndSvWonDiff+dfDiff+winDiff+UEdiff+bkWonDiff+bkPtFcDiff+forcedPointsDiff+unRetDiff,data = .)
-qdaModel<-train %>% qda(winner~fstSvWonDiff+sndSvWonDiff+dfDiff+winDiff+UEdiff+bkWonDiff+bkPtFcDiff+forcedPointsDiff+unRetDiff,data = .)
-
-treeModel<-tree(winner~fstSvWonDiff+sndSvWonDiff+dfDiff+winDiff+UEdiff+bkWonDiff+bkPtFcDiff+forcedPointsDiff+unRetDiff,data = train)
-plot(treeModel)
-text(treeModel,splits = T,digits = 3)
-
-pLog<-round(predict(logModel,test,type='response'))
-mean(pLog==test$winner)
-
-pLDA<-predict(ldaModel,test,type='response')$class
-mean(pLDA==test$winner)
-
-pTree<-predict(treeModel,test,type='class')
-mean(pTree==test$winner)
-
-pQDA<-predict(qdaModel,test,type='response')$class
-mean(pQDA==test$winner)
-logModel
-atpMatchesDiff$dfDiff
-
-# diff and no diff 
